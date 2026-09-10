6929e18a1e55eccd779df5e43d735c9a6c402f8a
zainab-ali <zainab@kebab-ca.se>
2026-09-10 17:58:16 +0100

    Update laika version.

--- site/build.sbt
+++ site/build.sbt
@@ -1,3 +1,3 @@
 addSbtPlugin("org.scalameta" % "sbt-mdoc" % "2.9.0")
-// This is a locally published version.
-addSbtPlugin("org.typelevel" % "laika-sbt" % "1.1.1")
+// TODO: Zainab - This is a locally published version.
+addSbtPlugin("org.typelevel" % "laika-sbt" % "1.3.1-79-7bf5db5-SNAPSHOT")
--- site/src/main/scala/org/typelevel/sbt/TypelevelSitePlugin.scala
+++ site/src/main/scala/org/typelevel/sbt/TypelevelSitePlugin.scala
@@ -114,6 +114,8 @@ object TypelevelSitePlugin extends AutoPlugin {
       )
       .value: @nowarn(),
     tlSitePreview := previewTask.value,
+    // TODO: Zainab - mdocOut needs to exist before laikaSite is run. This should happen with Def.sequential.
+      // Perhaps the way source directories are specified is incorrect?
     Laika / sourceDirectories := Seq(mdocOut.value),
     laikaTheme := tlSiteHelium.value.build,
     mdocVariables := {
