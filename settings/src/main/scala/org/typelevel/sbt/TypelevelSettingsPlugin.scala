83640b29c588b079b9c946b70c44c5abcbe5cef4
zainab-ali <zainab@kebab-ca.se>
2026-09-14 13:45:46 +0100

    Remove TODO on scalac-compat-annotation.

--- settings/src/main/scala/org/typelevel/sbt/TypelevelSettingsPlugin.scala
+++ settings/src/main/scala/org/typelevel/sbt/TypelevelSettingsPlugin.scala
@@ -66,7 +66,6 @@ object TypelevelSettingsPlugin extends AutoPlugin {
               ("org.typelevel" % "kind-projector" % "0.13.4").cross(CrossVersion.full))
           )
 
-      // TODO: Zainab - This is causing problems in discipline build for JS and Native.
       val scalacCompat =
         if (Set("2.12", "2.13", "3").contains(scalaBinaryVersion.value))
           Seq(PluginCompat.jvmPlatform("org.typelevel" %% "scalac-compat-annotation" % "0.1.4" % Provided))
